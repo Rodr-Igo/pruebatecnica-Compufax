9edea344aca2b69c2d45e753e9c4f61490564004
Rodrigo Daniel Euan Briceño <rodrigo981913@gmail.com>
2025-04-03 18:22:22 -0600

    Creado direcciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'DB/data-source'
 import { ClientModule } from './client/client.module';
+import { DireccionesModule } from './direcciones/direcciones.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), ClientModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), ClientModule, DireccionesModule],
   controllers: [],
   providers: [],
 })
