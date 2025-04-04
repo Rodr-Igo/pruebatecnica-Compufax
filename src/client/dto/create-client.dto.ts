3c2a90bfdbf832bfb2351f266c333813583822c9
Rodrigo Daniel Euan Briceño <rodrigo981913@gmail.com>
2025-04-03 18:53:12 -0600

    Agregando validation pipe para excepciones

--- src/client/dto/create-client.dto.ts
+++ src/client/dto/create-client.dto.ts
@@ -1,1 +1,16 @@
-export class CreateClientDto {}
+import { IsString, IsInt, IsEmail, Min } from 'class-validator';
+
+export class CreateClientDto {  
+    @IsString()
+    nombre: string;
+  
+    @IsString()
+    apellido: string;
+  
+    @IsInt()
+    @Min(0)
+    edad: number;
+  
+    @IsEmail()
+    email: string;
+  }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // elimina propiedades no definidas en DTOs
+      forbidNonWhitelisted: true, // lanza error si se envían propiedades extras
+      transform: true, // convierte tipos automáticamente
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
